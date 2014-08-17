590ce3758d56828d0bfb9f4c3f6de6ccb9e2d329
Ian Schulz <ian@schulz.local>
2014-08-17 14:15:25 +0200

    First Progressbar is working.

--- src/de/nrw/lichtenau/ian/db_copy/gui/Window.java
+++ src/de/nrw/lichtenau/ian/db_copy/gui/Window.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
@@ -30,10 +32,13 @@ import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JToolBar;
 import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 import javax.swing.border.BevelBorder;
 
 import de.nrw.lichtenau.ian.db_copy.ConfUtil;
 import de.nrw.lichtenau.ian.db_copy.DBProp;
+
+import javax.swing.JCheckBox;
 
 public class Window {
 
@@ -41,6 +46,11 @@ public class Window {
 	final JList<DBProp> list = new JList<>();
 	private JComboBox<DBProp> sourceDBcomboBox;
 	private JComboBox<DBProp> TargetDBcomboBox;
+	private JProgressBar currenttableprogressBar;
+	private JProgressBar allprogressBar;
+	private JCheckBox chckbxTruncateTables;
+	
+	private DBCopyTask copyTask;
 
 	/**
 	 * Launch the application.
@@ -171,14 +181,17 @@ public class Window {
 
 		TargetDBcomboBox = new JComboBox<>();
 		panel_2.add(TargetDBcomboBox);
+		
+		chckbxTruncateTables = new JCheckBox("Zieltabellen löschen");
+		panel_2.add(chckbxTruncateTables);
 
 		JPanel panel_3 = new JPanel();
 		panel_1.add(panel_3);
 
-		final JProgressBar currenttableprogressBar = new JProgressBar();
+		currenttableprogressBar = new JProgressBar();
 		panel_3.add(currenttableprogressBar);
 
-		JProgressBar allprogressBar = new JProgressBar();
+		allprogressBar = new JProgressBar();
 		panel_3.add(allprogressBar);
 
 		JPanel panel_4 = new JPanel();
@@ -187,97 +200,10 @@ public class Window {
 		JButton btnNewButton = new JButton("Kopieren");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				DBProp sconn = (DBProp) sourceDBcomboBox.getSelectedItem();
-				DBProp tconn = (DBProp) TargetDBcomboBox.getSelectedItem();
-				try {
-					Class.forName(((DBProp) tconn).getDriver());
-					Class.forName(((DBProp) sconn).getDriver());
-
-					try (
-							Connection scon = DriverManager.getConnection(sconn.getUrl(), sconn.getUser(), sconn.getPass());
-							Connection tcon = DriverManager.getConnection(tconn.getUrl(), tconn.getUser(), tconn.getPass());
-						) {
-						DatabaseMetaData smeta = scon.getMetaData();
-						DatabaseMetaData tmeta = tcon.getMetaData();
-						try (ResultSet sres = smeta.getTables(null, null, "%", null)) {
-							while (sres.next()) {
-								String tabellenname = sres.getString("table_name");
-								try (ResultSet tres = tmeta.getTables(null, null, tabellenname, null)) {
-									if (tres.next()) {
-										//select count('x' from tabellename)
-										
-										currenttableprogressBar.setMaximum(getRowCount(scon, tabellenname));
-										currenttableprogressBar.setValue(0);
-										
-										List<String> sColumnNames = getColumnNames(tabellenname, smeta);
-										List<String> tColumnNames = getColumnNames(tabellenname, tmeta);
-										List<String> stColumnNames = new ArrayList<String>();
-
-										for (String temp : sColumnNames) {
-											for (String temp2 : tColumnNames) {
-												if (temp.equals(temp2)) {
-													stColumnNames.add(temp);
-												}
-											}
-										}
-//										JOptionPane.showMessageDialog(frmDbCopy, stColumnNames);
-										
-//										select Feld1, Feld2, ..., Feld n from Tabellenname;
-//										insert into Tabellenname Feld1, Feld2, ..., Feld n values value1, value2, ..., value n;
-										Statement select = scon.createStatement();
-										String felderliste="";
-										String fragezeichenliste = "";
-										for(String columnName : stColumnNames) {
-											felderliste += ", ";
-											felderliste += columnName;
-											fragezeichenliste += ",?";
-										}
-										felderliste = felderliste.replaceFirst(",", "");
-										fragezeichenliste = fragezeichenliste.replaceFirst(",", "");
-										try (ResultSet cont = select.executeQuery("select "+felderliste+" from "+tabellenname)) {
-											//1, Königstr., 18, 33165,
-											//insert into tabellenname (Feld1, Feld2, ..., Feld n) values (1, 'Königstr', 18, 33165, ...)
-											//insert into tabellenname (Feld1, Feld2, ..., Feld n) values (?,?,? , ...)
-											
-											PreparedStatement insert = tcon.prepareStatement("insert into "+tabellenname+" ("+felderliste+") values ("+fragezeichenliste+")");
-											while (cont.next()) {
-												for(int i = 1 ; i <= stColumnNames.size(); i++) {
-													insert.setObject(i,cont.getObject(i));
-												}
-												
-										        SwingUtilities.invokeLater(new Runnable() {
-										            @Override
-										            public void run() {
-														currenttableprogressBar.setValue(currenttableprogressBar.getValue()+1);
-										            }
-										        });
-												insert.execute();
-											}
-										}
-									}else {
-										System.out.println("Tabelle '"+tabellenname+"' ist in Zieldatenbank nicht vorhanden");
-									}
-								}
-							}
-						}
-					} catch (SQLException e) {
-						JOptionPane.showMessageDialog(frmDbCopy, e.getMessage());
-						e.printStackTrace();
-					}
-				} catch (ClassNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				copyTask=new DBCopyTask(Window.this);
+				copyTask.execute();
+				System.out.println("nach new DBCopyTask.execute()");
 			}
-
-			private int getRowCount(Connection scon, String tabellenname) throws SQLException {
-				Statement select = scon.createStatement();
-				ResultSet rowCount = select.executeQuery("select count('x') from "+tabellenname);
-				rowCount.next();
-				return rowCount.getInt(1);
-				
-			}
-
 		});
 		panel_4.add(btnNewButton);
 
@@ -309,14 +235,139 @@ public class Window {
 		return TargetDBcomboBox;
 	}
 
-	private List<String> getColumnNames(String tabellenname, DatabaseMetaData meta) throws SQLException {
-		List<String> columnNames = new ArrayList<>(); 
-		try (ResultSet res2 = meta.getColumns(null, null, tabellenname, "%")) {
-
-			while (res2.next()) {
-				columnNames.add(res2.getString("column_name"));
-			}
+	public JProgressBar getCurrenttableprogressBar() {
+		return currenttableprogressBar;
+	}
+	
+	public JProgressBar getAllprogressBar() {
+		return allprogressBar;
+	}
+	
+	private static final class DBCopyTask extends SwingWorker<Integer, Integer> {
+		private Window invoker;
+		
+		DBCopyTask(Window invoker) {
+			this.invoker=invoker;
 		}
-		return columnNames;
+		
+		@Override
+		protected Integer doInBackground() throws Exception {
+			System.out.println("in DBCopyTask.run()");
+			DBProp sconn = (DBProp) invoker.sourceDBcomboBox.getSelectedItem();
+			DBProp tconn = (DBProp) invoker.TargetDBcomboBox.getSelectedItem();
+			try {
+				Class.forName(((DBProp) tconn).getDriver());
+				Class.forName(((DBProp) sconn).getDriver());
+
+				try (
+						Connection scon = DriverManager.getConnection(sconn.getUrl(), sconn.getUser(), sconn.getPass());
+						Connection tcon = DriverManager.getConnection(tconn.getUrl(), tconn.getUser(), tconn.getPass());
+					) {
+					DatabaseMetaData smeta = scon.getMetaData();
+					DatabaseMetaData tmeta = tcon.getMetaData();
+					try (ResultSet sres = smeta.getTables(null, null, "%", null)) {
+						while (sres.next()) {
+							String tabellenname = sres.getString("table_name");
+							try (ResultSet tres = tmeta.getTables(null, null, tabellenname, null)) {
+								if (tres.next()) {
+									if(invoker.chckbxTruncateTables.isSelected()) {
+										Statement truncate=tcon.createStatement();
+										truncate.executeUpdate("truncate table "+tabellenname);
+									}
+									invoker.currenttableprogressBar.setMaximum(getRowCount(scon, tabellenname));
+									invoker.currenttableprogressBar.setValue(0);
+					            	invoker.currenttableprogressBar.setString(invoker.currenttableprogressBar.getValue()+"/"+invoker.currenttableprogressBar.getMaximum());
+									invoker.currenttableprogressBar.setStringPainted(true);
+									System.out.println("Progressbar initialisiert ...");
+									
+									List<String> sColumnNames = getColumnNames(tabellenname, smeta);
+									List<String> tColumnNames = getColumnNames(tabellenname, tmeta);
+									List<String> stColumnNames = new ArrayList<String>();
+
+									for (String temp : sColumnNames) {
+										for (String temp2 : tColumnNames) {
+											if (temp.equals(temp2)) {
+												stColumnNames.add(temp);
+											}
+										}
+									}
+//									JOptionPane.showMessageDialog(frmDbCopy, stColumnNames);
+									
+//									select Feld1, Feld2, ..., Feld n from Tabellenname;
+//									insert into Tabellenname Feld1, Feld2, ..., Feld n values value1, value2, ..., value n;
+									Statement select = scon.createStatement();
+									String felderliste="";
+									String fragezeichenliste = "";
+									for(String columnName : stColumnNames) {
+										felderliste += ", ";
+										felderliste += columnName;
+										fragezeichenliste += ",?";
+									}
+									felderliste = felderliste.replaceFirst(",", "");
+									fragezeichenliste = fragezeichenliste.replaceFirst(",", "");
+									try (ResultSet cont = select.executeQuery("select "+felderliste+" from "+tabellenname)) {
+										//1, Königstr., 18, 33165,
+										//insert into tabellenname (Feld1, Feld2, ..., Feld n) values (1, 'Königstr', 18, 33165, ...)
+										//insert into tabellenname (Feld1, Feld2, ..., Feld n) values (?,?,? , ...)
+										
+										PreparedStatement insert = tcon.prepareStatement("insert into "+tabellenname+" ("+felderliste+") values ("+fragezeichenliste+")");
+										while (cont.next()) {
+											for(int i = 1 ; i <= stColumnNames.size(); i++) {
+												insert.setObject(i,cont.getObject(i));
+											}
+											
+//									        SwingUtilities.invokeLater(new Runnable() {
+//									            @Override
+//									            public void run() {
+//									            	invoker.currenttableprogressBar.setValue(invoker.currenttableprogressBar.getValue()+1);
+//									            	System.out.println("in invokeLater Progressbar-Update ...");
+//									            }
+//									        });
+//									        System.out.println("nach invokeLater Progressbar-Update ...");
+							            	invoker.currenttableprogressBar.setValue(invoker.currenttableprogressBar.getValue()+1);
+							            	invoker.currenttableprogressBar.setString(invoker.currenttableprogressBar.getValue()+"/"+invoker.currenttableprogressBar.getMaximum());
+//									        System.out.println("nach Progressbar-Update ...");
+							            	insert.execute();
+										}
+									}
+								}else {
+									System.out.println("Tabelle '"+tabellenname+"' ist in Zieldatenbank nicht vorhanden");
+								}
+							}
+						}
+					}
+				} catch (SQLException e) {
+					JOptionPane.showMessageDialog(invoker.frmDbCopy, e.getMessage());
+					e.printStackTrace();
+				}
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return 0;
+		}
+		
+		private List<String> getColumnNames(String tabellenname, DatabaseMetaData meta) throws SQLException {
+			List<String> columnNames = new ArrayList<>(); 
+			try (ResultSet res2 = meta.getColumns(null, null, tabellenname, "%")) {
+
+				while (res2.next()) {
+					columnNames.add(res2.getString("column_name"));
+				}
+			}
+			return columnNames;
+		}
+		
+		private int getRowCount(Connection scon, String tabellenname) throws SQLException {
+			Statement select = scon.createStatement();
+			ResultSet rowCount = select.executeQuery("select count('x') from "+tabellenname);
+			rowCount.next();
+			return rowCount.getInt(1);
+			
+		}
+
+	}
+	public JCheckBox getChckbxTruncateTables() {
+		return chckbxTruncateTables;
 	}
 }
